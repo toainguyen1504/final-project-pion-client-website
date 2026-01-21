a412c002546dc5b327a91623ba4c1097bc48c3c2
toainguyen1504 <toaincgcss200874@fpt.edu.vn>
2026-01-21 22:39:20 +0700

    feat: optimize ui and code for auth

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getCurrentUser, isAuthenticated, logout } from '@/services/authService';
+import config from '@/config';
+
+export default function useAuth() {
+    const [user, setUser] = useState(null);
+    const isAuth = isAuthenticated();
+
+    useEffect(() => {
+        if (isAuth) {
+            setUser(getCurrentUser());
+        } else {
+            setUser(null);
+        }
+    }, [isAuth]);
+
+    const handleLogout = () => {
+        logout();
+        window.location.href = config.routes.login;
+    };
+
+    return {
+        isAuth,
+        user,
+        logout: handleLogout,
+    };
+}
---
+++ src/layouts/components/Header/components/DesktopAuth.jsx
@@ -0,0 +1,83 @@
+import { Avatar, Badge, Dropdown } from 'antd';
+import { IoMdNotifications } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import { getInitial } from '@/utils';
+import styles from '../Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+export default function DesktopAuth({ user, onLogout }) {
+    return (
+        <div className={cx('auth-inner')}>
+            {/* Notification */}
+            <div className={cx('notification')}>
+                <Badge count={99} overflowCount={99}>
+                    <IoMdNotifications size={26} />
+                </Badge>
+            </div>
+
+            {/* User dropdown */}
+            <Dropdown
+                trigger={['click']}
+                placement="bottomRight"
+                overlayClassName={cx('auth-dropdown')}
+                menu={{
+                    items: [
+                        {
+                            key: 'user-info',
+                            label: (
+                                <div className={cx('user-info-desktop')}>
+                                    <div className={cx('avatar-border')}>
+                                        <Avatar size={48} src={user.profile_image} className={cx('avatar')}>
+                                            {!user.profile_image && getInitial(user.display_name)}
+                                        </Avatar>
+                                    </div>
+
+                                    <div className={cx('user-text')}>
+                                        <p className={cx('display-name')}>{user.display_name}</p>
+                                        <p className={cx('username')}>@{user.username}</p>
+                                    </div>
+                                </div>
+                            ),
+                            disabled: true,
+                        },
+                        { type: 'divider' },
+                        {
+                            key: 'learning',
+                            label: (
+                                <Link to="/learning" className={cx('link')}>
+                                    Vào học
+                                </Link>
+                            ),
+                        },
+                        { type: 'divider' },
+                        {
+                            key: 'profile',
+                            label: (
+                                <Link to="/profile" className={cx('link')}>
+                                    Cập nhật hồ sơ
+                                </Link>
+                            ),
+                        },
+                        { type: 'divider' },
+                        {
+                            key: 'logout',
+                            label: (
+                                <span className={cx('logout', 'link')} onClick={onLogout}>
+                                    Đăng xuất
+                                </span>
+                            ),
+                        },
+                    ],
+                }}
+            >
+                <div className={cx('avatar-border')}>
+                    <Avatar size={28} className={cx('trigger-avatar')} src={user.profile_image}>
+                        {!user.profile_image && getInitial(user.display_name)}
+                    </Avatar>
+                </div>
+            </Dropdown>
+        </div>
+    );
+}
